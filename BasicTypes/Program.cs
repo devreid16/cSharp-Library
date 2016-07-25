e32a44d4425c45149762f0569c8ccc0d74298429
Angela Reid <reidang94@gmail.com>
2016-07-25 11:14:44 -0400

    add integer demo project

--- BasicTypes/Program.cs
+++ BasicTypes/Program.cs
@@ -11,36 +11,15 @@ namespace BasicTypes
         //String data type demo
         static void Main(string[] args)
         {
-        //   string animal = "Dog";
-        //   string name = "Nicole";
-        //   string activity = "walk";
+            string animal = "Dog";
+            string name = "Nicole";
+            string activity = "walk";
 
-        //  //  var sentence = $" {name} + {animal} + {activity}";
+            //  var sentence = $" {name} + {animal} + {activity}";
 
-        //    Console.WriteLine($" {name} takes her {animal} for a {activity}.");
-        //      Console.ReadLine();
-        //Integer data type demo
-            int age = 50;
-            double price = 13.56;
-            decimal gpa = 3.89m;
-            short tot = 566;
-            long num = 7;
-            byte b = 1;
-            char a = 'i';
-            bool guess = false;
-            float points = 3.578f;
+            Console.WriteLine($" {name} takes her {animal} for a {activity}.");
+            Console.ReadLine();
 
-           // Console.WriteLine($"Tony's age is {age} this year.  Sydney has {b} more year of school, and her gpa of {gpa} is fine.");
-           // Console.ReadLine();
-                        
-            Console.WriteLine($"Justin paid {price} for his lunch. His dog is {num} years old.");
-            Console.ReadLine();
-                        
-            Console.WriteLine($"We drove {tot} miles in one day.  They were {guess} is their estimate.");
-            Console.ReadLine();
-                       
-            Console.WriteLine($"Sam's morgage rate is at {points} percent.");
-            Console.ReadLine();
         }
     }
    
---
+++ IntegerDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Integer data type demo
+            int age = 50;            
+            decimal gpa = 3.89m;
+            short tot = 566;
+            double price = 13.56;
+            long num = 7;
+            byte b = 1;
+            char a = 'i';
+            bool guess = false;
+            float points = 3.578f;
+
+              Console.WriteLine($"Tony's age is {age} this year.  Sydney has {b} more year of school, and her gpa of {gpa} is fine.");
+             
+              Console.WriteLine($"Justin paid {price} for his lunch. His dog is {num} years old.");
+             
+              Console.WriteLine($"We drove {tot} miles in one day.  They were {guess} is their estimate.");
+            
+              Console.WriteLine($"Sam's morgage rate is at {points} percent.");
+              Console.ReadLine();
+        }
+    }
+}
