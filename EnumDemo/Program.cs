83c34fdc449a86de95c68d6ad6f20c7fa0fc444f
Angela Reid <reidang94@gmail.com>
2016-07-25 12:26:00 -0400

    added EnumDemo

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    class Program
+    {
+        enum Fitness
+        {
+            Kickboxing,
+            Refit,
+            Running,
+            Walking,
+            Treadmill,
+            Elliptical,
+            Weights
+        }
+
+        static void Main(string[] args)
+        {         
+            Fitness FavWorkout = Fitness.Kickboxing;
+
+            Console.WriteLine($"My favorite fitness activity is: { FavWorkout} .");
+            Console.ReadLine();
+        }
+    }
+}
+
+ 
