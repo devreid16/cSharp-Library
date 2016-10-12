6a05e115c0c245d480e0eb36f43f1721e23f46d6
Angela Reid <reidang94@gmail.com>
2016-10-12 16:57:49 -0400

    collections, list, generics examples

--- CollectionsDemo/Program.cs
+++ CollectionsDemo/Program.cs
@@ -10,94 +10,145 @@ namespace CollectionsDemo
     {
           static void Main(string[] args)
           {
-               //string list of favorite fruits
-               //var fruits = new List<string>();
-               //    fruits.Add("Cherries");
-               //    fruits.Add("Grapes");
-               //    fruits.Add("Bananas");
-               //    fruits.Add("Peaches");
-               //    fruits.Add("Apples");
+            //string list of favorite fruits
+            //var fruits = new List<string>();
+            //    fruits.Add("Cherries");
+            //    fruits.Add("Grapes");
+            //    fruits.Add("Bananas");
+            //    fruits.Add("Peaches");
+            //    fruits.Add("Apples");
 
-               // //set x counter to zero, compare counter to number of fruits listed, as long as there are
-               // //more fruits add 1 to counter
-               //for (var x = 0; x < fruits.Count; x++)
-               //{
-               //    Console.WriteLine(fruits[x]); //set variable food equal to the current fruit using the index
+            // //set x counter to zero, compare counter to number of fruits listed, as long as there are
+            // //more fruits add 1 to counter
+            //for (var x = 0; x < fruits.Count; x++)
+            //{
+            //    Console.WriteLine(fruits[x]); //set variable food equal to the current fruit using the index
 
-               //}
+            //}
 
-               //for (var x = 0; x < fruits.Count; x++)
-               //{
-               //    Console.WriteLine(fruits); //print list of foods using interpolation
-               //    Console.ReadLine(); //pause screen to read
-               //}
+            //for (var x = 0; x < fruits.Count; x++)
+            //{
+            //    Console.WriteLine(fruits); //print list of foods using interpolation
+            //    Console.ReadLine(); //pause screen to read
+            //}
 
-               //string[] friend = { "Norma", "Sarah", "BettySue", "Donna" };
+            //string[] friend = { "Norma", "Sarah", "BettySue", "Donna" };
 
-               //foreach(string name in friend)
-               //{
-               //    Console.WriteLine(name);                
-               //}
-               //Console.WriteLine("Press Enter to Exit");
-               //Console.ReadLine();
+            //foreach(string name in friend)
+            //{
+            //    Console.WriteLine(name);                
+            //}
+            //Console.WriteLine("Press Enter to Exit");
+            //Console.ReadLine();
 
-               //Fixed Value Array : 
-               //double[] doublesArray = {}5,2, 7, 3.5. 6.5, 8, 1, 9, 1, 3;
+            //Fixed Value Array : 
+            //double[] doublesArray = {}5,2, 7, 3.5. 6.5, 8, 1, 9, 1, 3;
 
-               //VariableAverageArray
-               //Console.WriteLine("Enter the number of values to average");
+            //VariableAverageArray
+            //Console.WriteLine("Enter the number of values to average");
 
-               //string numElementsInput = Console.ReadLine();
+            //string numElementsInput = Console.ReadLine();
 
-               //int numElements = Convert.ToInt32(numElementsInput);
-               //Console.WriteLine(); //skip line
+            //int numElements = Convert.ToInt32(numElementsInput);
+            //Console.WriteLine(); //skip line
 
-               ////declare array of that size
+            ////declare array of that size
 
-               //double[] doublesArray = new double[numElements];
-                              
-               //for (int i = 0; i < numElements; i++)
-               //{
-               //     Console.WriteLine("Enter a double #" + (i + 1) + ":");
-               //     string val = Console.ReadLine();
+            //double[] doublesArray = new double[numElements];
 
-               //     //add to array
-               //     double value = Convert.ToDouble(val);
-               //     doublesArray[i] = value;                    
-               //}
+            //for (int i = 0; i < numElements; i++)
+            //{
+            //     Console.WriteLine("Enter a double #" + (i + 1) + ":");
+            //     string val = Console.ReadLine();
 
-               ////accumulate values 
+            //     //add to array
+            //     double value = Convert.ToDouble(val);
+            //     doublesArray[i] = value;                    
+            //}
 
-               //double sum = 0;
+            ////accumulate values 
 
-               //for (int i = 0; i < numElements; i++)
-               //{
-               //     sum = sum + doublesArray[i];
-               //}
+            //double sum = 0;
 
-               ////calculate average
-               //double average = (sum / numElements);
+            //for (int i = 0; i < numElements; i++)
+            //{
+            //     sum = sum + doublesArray[i];
+            //}
 
-               //Console.WriteLine(); //skip line
-               //Console.WriteLine(average + " is the average of (" + doublesArray[0]);
+            ////calculate average
+            //double average = (sum / numElements);
 
-               //for (int i = 1; i < numElements; i++)
-               //{
-               //     Console.WriteLine("+" + doublesArray[i]);
-               //}
+            //Console.WriteLine(); //skip line
+            //Console.WriteLine(average + " is the average of (" + doublesArray[0]);
 
-               //Console.WriteLine(") / " + numElements);
+            //for (int i = 1; i < numElements; i++)
+            //{
+            //     Console.WriteLine("+" + doublesArray[i]);
+            //}
 
-               //Console.WriteLine("Press Enter to exit");
-               //Console.Read();
+            //Console.WriteLine(") / " + numElements);
+
+            //Console.WriteLine("Press Enter to exit");
+            //Console.Read();
+
+            //generic types provide basic functionality of a specific type
+            //List is a generic collection until you make it specific by giving it a type
+            //List<int> smallPrimeNumbers = new List<int> { 2, 3, 5, 7, 11, 13 };
+
+            //   int numberElements = smallPrimeNumbers.Count;
+            //   Console.WriteLine(numberElements);
+            //   Console.ReadLine();
 
 
-               //List collection
-               List<int> smallPrimeNumbers = new List<int> { 2, 3, 5, 7, 11, 13 };
+            //car objects
+            Car car1 = new Car();
+            car1.Make = "Oldsmobile";
+            car1.Model = "Cutlass Supreme";
 
-               int numberElements = smallPrimeNumbers.Count;
-               Console.WriteLine(numberElements);
-               Console.ReadLine();
-          }
+            Car car2 = new Car();
+            car2.Make = "Ford";
+            car2.Model = "Pinto";
+
+            //examples       of collections    
+           // System.Collections.ArrayList myArrayList = new System.Collections.ArrayList();
+           // System.Collections.Specialized.ListDictionary myDictionary
+          //      = new System.Collections.Specialized.ListDictionary();
+
+            List<Car> myList = new List<Car>();
+            myList.Add(car1);
+            myList.Add(car2);
+
+            foreach (Car car in myList)
+            {
+                //no casting required
+                Console.WriteLine(car.Model);
+            }
+
+
+            //Dictionary
+
+            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
+            myDictionary.Add(car1.Make, car1);
+            myDictionary.Add(car2.Make, car2);
+
+
+            //collection initializer
+          //  Car car1 = new Car() { Make = "Oldsmobile", Model = "Cutlass Supreme" };
+
+
+
+            Console.WriteLine(myDictionary["Ford"].Model);
+
+            Console.ReadLine();
+
+
+
+        }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+        }
     }
 }
