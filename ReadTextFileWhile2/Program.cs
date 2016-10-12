0c1c65505fddbdc9df49033967d9e042bc5b3a0f
Angela Reid <reidang94@gmail.com>
2016-10-12 14:43:45 -0400

    exception handling

--- ReadTextFileWhile2/Program.cs
+++ ReadTextFileWhile2/Program.cs
@@ -11,17 +11,42 @@ namespace ReadTextFileWhile2
     {
         static void Main(string[] args)
         {
-            StreamReader myReader = new StreamReader("Values.txt");
-            string line = "";
+            //Exeception handling
+            try
+            {               
+                StreamReader myReader = new StreamReader("Values.txt");
+                string line = "";
 
-            while(line != null)
-            {
-                line = myReader.ReadLine();
-                if (line != null)
-                    Console.WriteLine(line);
+                while (line != null)
+                {
+                    line = myReader.ReadLine();
+                    if (line != null)
+                        Console.WriteLine(line);
+                }
+
+                myReader.Close();
+                
             }
 
-            myReader.Close();
+            //catch the exception
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Couldn't find the file.  Are you sure the DIRECTORY exists?");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Couldn't find the file.  Are you sure you're looking for right file?");
+            }
+            
+            catch (Exception e)
+            {       //display message with error message
+                Console.WriteLine("Something didn't quite work correctly: {0}", e.Message);
+            }
+            finally
+            {
+                //Perform any cleanup to roll back the data or close connections
+                //to files, database, network, etc.
+            }
             Console.ReadLine();
         }
     }
